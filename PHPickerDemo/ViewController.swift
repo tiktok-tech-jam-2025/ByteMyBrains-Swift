b8c230244a5ae833303150ec50601748dff4259e
Apple <>
2021-05-15 20:52:20 -0800

    Updates to the README

--- PHPickerDemo/ViewController.swift
+++ PHPickerDemo/ViewController.swift
@@ -7,19 +7,28 @@ The app's main view controller object.
 
 import UIKit
 import PhotosUI
+import AVKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var livePhotoView: PHLivePhotoView! {
         didSet {
             livePhotoView.contentMode = .scaleAspectFit
         }
     }
+    @IBOutlet weak var playButton: UIButton!
+    private var playButtonVideoURL: URL?
+
+    private var selection = [String: PHPickerResult]()
+    private var selectedAssetIdentifiers = [String]()
+    private var selectedAssetIdentifierIterator: IndexingIterator<[String]>?
+    private var currentAssetIdentifier: String?
     
-    private var itemProviders = [NSItemProvider]()
-    private var itemProvidersIterator: IndexingIterator<[NSItemProvider]>?
-    private var currentItemProvider: NSItemProvider?
+    @IBAction func presentPickerForImagesAndVideos(_ sender: Any) {
+        presentPicker(filter: nil)
+    }
     
     @IBAction func presentPickerForImagesIncludingLivePhotos(_ sender: Any) {
         presentPicker(filter: PHPickerFilter.images)
@@ -29,71 +38,184 @@ class ViewController: UIViewController {
         presentPicker(filter: PHPickerFilter.livePhotos)
     }
     
-    private func presentPicker(filter: PHPickerFilter) {
-        var configuration = PHPickerConfiguration()
+    /// - Tag: PresentPicker
+    private func presentPicker(filter: PHPickerFilter?) {
+        var configuration = PHPickerConfiguration(photoLibrary: .shared())
+        
+        // Set the filter type according to the user’s selection.
         configuration.filter = filter
+        // Set the mode to avoid transcoding, if possible, if your app supports arbitrary image/video encodings.
+        configuration.preferredAssetRepresentationMode = .current
+        // Set the selection behavior to respect the user’s selection order.
+        configuration.selection = .ordered
+        // Set the selection limit to enable multiselection.
         configuration.selectionLimit = 0
+        // Set the preselected asset identifiers with the identifiers that the app tracks.
+        configuration.preselectedAssetIdentifiers = selectedAssetIdentifiers
         
         let picker = PHPickerViewController(configuration: configuration)
         picker.delegate = self
         present(picker, animated: true)
     }
-        
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        displayNextImage()
+        displayNext()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let viewController = segue.destination as? AVPlayerViewController, let videoURL = playButtonVideoURL {
+            viewController.player = AVPlayer(url: videoURL)
+            viewController.player?.play()
+        }
+    }
+}
+
+private extension ViewController {
+    
+    /// - Tag: LoadItemProvider
+    func displayNext() {
+        guard let assetIdentifier = selectedAssetIdentifierIterator?.next() else { return }
+        currentAssetIdentifier = assetIdentifier
+        
+        let progress: Progress?
+        let itemProvider = selection[assetIdentifier]!.itemProvider
+        if itemProvider.canLoadObject(ofClass: PHLivePhoto.self) {
+            progress = itemProvider.loadObject(ofClass: PHLivePhoto.self) { [weak self] livePhoto, error in
+                DispatchQueue.main.async {
+                    self?.handleCompletion(assetIdentifier: assetIdentifier, object: livePhoto, error: error)
+                }
+            }
+        }
+        else if itemProvider.canLoadObject(ofClass: UIImage.self) {
+            progress = itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+                DispatchQueue.main.async {
+                    self?.handleCompletion(assetIdentifier: assetIdentifier, object: image, error: error)
+                }
+            }
+        } else if itemProvider.hasItemConformingToTypeIdentifier(UTType.movie.identifier) {
+            progress = itemProvider.loadFileRepresentation(forTypeIdentifier: UTType.movie.identifier) { [weak self] url, error in
+                do {
+                    guard let url = url, error == nil else {
+                        throw error ?? NSError(domain: NSFileProviderErrorDomain, code: -1, userInfo: nil)
+                    }
+                    let localURL = FileManager.default.temporaryDirectory.appendingPathComponent(url.lastPathComponent)
+                    try? FileManager.default.removeItem(at: localURL)
+                    try FileManager.default.copyItem(at: url, to: localURL)
+                    DispatchQueue.main.async {
+                        self?.handleCompletion(assetIdentifier: assetIdentifier, object: localURL)
+                    }
+                } catch let catchedError {
+                    DispatchQueue.main.async {
+                        self?.handleCompletion(assetIdentifier: assetIdentifier, object: nil, error: catchedError)
+                    }
+                }
+            }
+        } else {
+            progress = nil
+        }
+        
+        displayProgress(progress)
+    }
+    
+    func handleCompletion(assetIdentifier: String, object: Any?, error: Error? = nil) {
+        guard currentAssetIdentifier == assetIdentifier else { return }
+        if let livePhoto = object as? PHLivePhoto {
+            displayLivePhoto(livePhoto)
+        } else if let image = object as? UIImage {
+            displayImage(image)
+        } else if let url = object as? URL {
+            displayVideoPlayButton(forURL: url)
+        } else if let error = error {
+            print("Couldn't display \(assetIdentifier) with error: \(error)")
+            displayErrorImage()
+        } else {
+            displayUnknownImage()
+        }
     }
     
 }
 
 private extension ViewController {
-    func displayNextImage() {
-        guard let itemProvider = itemProvidersIterator?.next() else { return }
-        currentItemProvider = itemProvider
-        if itemProvider.canLoadObject(ofClass: PHLivePhoto.self) {
-            itemProvider.loadObject(ofClass: PHLivePhoto.self) { [weak self] livePhoto, error in
-                DispatchQueue.main.async {
-                    guard let self = self, self.currentItemProvider == itemProvider else { return }
-                    if let livePhoto = livePhoto as? PHLivePhoto {
-                        self.display(livePhoto: livePhoto)
-                    } else {
-                        self.display(image: UIImage(systemName: "exclamationmark.circle"))
-                        print("Couldn't load live photo with error: \(error?.localizedDescription ?? "unknown error")")
-                   }
-                }
-            }
-        } else if itemProvider.canLoadObject(ofClass: UIImage.self) {
-            itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
-                DispatchQueue.main.async {
-                    guard let self = self, self.currentItemProvider == itemProvider else { return }
-                    if let image = image as? UIImage {
-                        self.display(image: image)
-                    } else {
-                        self.display(image: UIImage(systemName: "exclamationmark.circle"))
-                        print("Couldn't load image with error: \(error?.localizedDescription ?? "unknown error")")
-                    }
-                }
-            }
-        } else {
-            print("Unsupported item provider: \(itemProvider)")
-        }
+    
+    func displayEmptyImage() {
+        displayImage(UIImage(systemName: "photo.on.rectangle.angled"))
     }
     
-    func display(livePhoto: PHLivePhoto? = nil, image: UIImage? = nil) {
+    func displayErrorImage() {
+        displayImage(UIImage(systemName: "exclamationmark.circle"))
+    }
+    
+    func displayUnknownImage() {
+        displayImage(UIImage(systemName: "questionmark.circle"))
+    }
+    
+    func displayProgress(_ progress: Progress?) {
+        imageView.image = nil
+        imageView.isHidden = true
+        livePhotoView.livePhoto = nil
+        livePhotoView.isHidden = true
+        playButtonVideoURL = nil
+        playButton.isHidden = true
+        progressView.observedProgress = progress
+        progressView.isHidden = progress == nil
+    }
+    
+    func displayVideoPlayButton(forURL videoURL: URL?) {
+        imageView.image = nil
+        imageView.isHidden = true
+        livePhotoView.livePhoto = nil
+        livePhotoView.isHidden = true
+        playButtonVideoURL = videoURL
+        playButton.isHidden = videoURL == nil
+        progressView.observedProgress = nil
+        progressView.isHidden = true
+    }
+    
+    func displayLivePhoto(_ livePhoto: PHLivePhoto?) {
+        imageView.image = nil
+        imageView.isHidden = true
         livePhotoView.livePhoto = livePhoto
         livePhotoView.isHidden = livePhoto == nil
+        playButtonVideoURL = nil
+        playButton.isHidden = true
+        progressView.observedProgress = nil
+        progressView.isHidden = true
+    }
+    
+    func displayImage(_ image: UIImage?) {
         imageView.image = image
         imageView.isHidden = image == nil
+        livePhotoView.livePhoto = nil
+        livePhotoView.isHidden = true
+        playButtonVideoURL = nil
+        playButton.isHidden = true
+        progressView.observedProgress = nil
+        progressView.isHidden = true
     }
+        
 }
 
 extension ViewController: PHPickerViewControllerDelegate {
-    
+    /// - Tag: ParsePickerResults
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         dismiss(animated: true)
         
-        itemProviders = results.map(\.itemProvider)
-        itemProvidersIterator = itemProviders.makeIterator()
-        displayNextImage()
+        let existingSelection = self.selection
+        var newSelection = [String: PHPickerResult]()
+        for result in results {
+            let identifier = result.assetIdentifier!
+            newSelection[identifier] = existingSelection[identifier] ?? result
+        }
+        
+        // Track the selection in case the user deselects it later.
+        selection = newSelection
+        selectedAssetIdentifiers = results.map(\.assetIdentifier!)
+        selectedAssetIdentifierIterator = selectedAssetIdentifiers.makeIterator()
+        
+        if selection.isEmpty {
+            displayEmptyImage()
+        } else {
+            displayNext()
+        }
     }
-    
 }
