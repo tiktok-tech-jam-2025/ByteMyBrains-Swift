d133b5a6f3b03b91a5bddb5c2954f6470177addd
yeo-menghan <yeomenghan1989@gmail.com>
2025-08-29 16:17:03 +0800

    add bounding box visusaliation

---
+++ PHPickerDemo/OCRVisualizationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OCRVisualizationViewController.swift
+//  PHPickerDemo
+//
+//  Created by Yeo Meng Han on 29/8/25.
+//  Copyright © 2025 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct OCRVisualizationViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OCRVisualizationViewController()
+}
